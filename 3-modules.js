a61fca9606e8978e0a2d0a3156d759c8340126fd
Cosmin Nicolae Cristea <cosmin@yonder.local>
2022-05-10 18:26:11 +0300

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,15 @@
+//Modules - Encapsulated Code (only share minimum)
+
+const names = require('./4-names')
+const  sayHi = require('./5-utils')
+const data = require('./6-akternative-flavor')
+require('./7-mind-grenade')
+
+/*
+console.log(data);
+console.log(names);
+*/
+
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter);
